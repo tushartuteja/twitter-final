0204952b6348c9f25f5d461473b8403d3958f780
Tushar Tuteja <ttuteja@gmail.com>
2017-02-11 13:02:08 +0530

    minor bug

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :tweet
+
+  validates :tweet_id, presence: true
+  validates :user_id, presence: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,6 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   has_many :likes
 
-  validates :tweet_id, presence: true
-  validates :user_id, presence: true
+ 
 
 end
