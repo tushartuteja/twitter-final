8c04c2ae53bf9d014b40842f9010aa8f1a334ca6
Tushar Tuteja <ttuteja@gmail.com>
2017-03-15 18:19:59 +0530

    added smooth scroll to top and tweet length

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,74 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function onBodyLoad(){
+
+	var toTop = document.getElementById('toTop');
+	var id = null;
+	var delta;
+	var height;
+	function scrollToTop(){
+		var y = window.scrollY;
+		if (y > height / 2){
+			delta = delta + 10;
+		}else{
+			delta = delta - 10;
+			delta = delta < 0 ? 10 : delta;
+		}
+		y = y - delta;
+		y = y > 0 ? y : 0;
+		window.scrollTo(0,y);
+		if (y === 0 ){
+			clearInterval(id);
+		}
+	}
+
+	toTop.addEventListener('click', function(event){
+		delta = 5;
+		height = window.scrollY;
+		event.preventDefault();
+		id = setInterval(scrollToTop,10);
+	});
+
+
+	var content = document.getElementById("content");
+	content.addEventListener('keyup', function(){
+		var length = content.value.length;
+		var letters = document.getElementById("letters");
+		letters.innerHTML = length;
+
+		if(length > 140){
+			letters.style.color = "red";
+		}else{
+			letters.style.color = "black";
+		}
+	});
+}
+
+ window.addEventListener("load", function(event) {
+   onBodyLoad();
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
