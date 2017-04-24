519efbd7b0d11f6e174676d24357256968487cd6
Tushar Tuteja <ttuteja@gmail.com>
2017-04-24 18:02:41 +0530

    minor bug

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
-  belongs_to :tweet, dependent: :destroy
-
+  belongs_to :tweet
   validates :tweet_id, presence: true
   validates :user_id, presence: true
 
